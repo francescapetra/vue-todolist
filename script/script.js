21598f1874fefaed6a63902004548f36457e599e
francescapetra <francescalaurapetra@gmail.com>
2021-03-26 14:51:56 +0100

    input e button

--- script/script.js
+++ script/script.js
@@ -2,10 +2,20 @@ var app = new Vue(
   {
     el:"#root",
     data:{
-      lista: ["Fare le valigie", "Prelevare soldi", "Andare in libreria"],
+      newElement:"",
+      list: ["FARE LE VALIGIE", "PRELEVARE IN BANCA", "ADARE IN LIBRERIA"],
     },
     methods:{
-
+      deleteElement: function(elementIndex){
+        this.list.splice(elementIndex,1);
+        //console.log(index);
+      },
+      addElement: function(){
+        if (this.newElement!== "") {
+          this.list.push(this.newElement.toUpperCase());
+          this.newElement = "";
+        }
+      }
     }
   }
 );
